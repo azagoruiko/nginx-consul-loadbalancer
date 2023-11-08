0b478f834511f69adfa6ac93c1d7ca9fc3934d88
Andrii Zagoruiko <andrii.zagoruiko@oracle.com>
2023-11-08 11:40:08 +0100

    remove minio

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 VER=$1
 
-docker build -t "10.8.0.5:5000/docker/consul-template-nginx:${VER}" .
-docker push "10.8.0.5:5000/docker/consul-template-nginx:${VER}"
+docker build -t "10.8.0.5:5000/consul-template-nginx:${VER}" .
+docker push "10.8.0.5:5000/consul-template-nginx:${VER}"
 export NOMAD_ADDR="http://10.8.0.1:4646"
 nomad job run nginx-consul-loadbalancer.nomad
